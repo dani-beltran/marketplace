1e5f5717112237dfe09debe6a4e640d36f85d8e7
Daniel Beltran <danilidonbeltran@gmail.com>
2023-03-30 15:36:36 +0200

    added next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ email }) {
+      // Check if the user is allowed to sign in
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      // The redirect callback is called anytime the user is redirected to a 
+      // callback URL (e.g. on signin or signout).
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+    async jwt({ token, user, account, profile }) {
+      // Use this callback to add custom properties to the JWT
+      return token;
+    },
+    async session({ session }) {
+      // Use this callback to send properties to the client through the session.
+      return session;
+    },
+  },
+  secret: process.env.JWT_SECRET!,
+});
---
+++ src/pages/login.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const login = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <p>Welcome, {session.user?.email}</p>
+        <img src={session.user?.image!} alt="user image" style={{borderRadius: '50px', height: '50px', width: '50px'}}/>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>You are not signed in.</p>
+        <button onClick={() => signIn()}>Sign in</button>
+      </div>
+    );
+  }
+};
+
+export default login;
