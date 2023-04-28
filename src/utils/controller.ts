2f6b1a1c76b51635e100c9ed931a9e3aa7f6513c
Daniel Beltran <danilidonbeltran@gmail.com>
2023-04-28 10:22:52 +0200

    refactor error handling in controller

--- src/utils/controller.ts
+++ src/utils/controller.ts
@@ -10,7 +10,7 @@ import {
 import { Dictionary } from "lodash";
 import ActionError from "./ActionError";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../pages/api/auth/[...nextauth]"
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 import { Session } from "next-auth";
 
 type ControllerParams<T_Input, T_Output> = {
@@ -21,16 +21,14 @@ type ControllerParams<T_Input, T_Output> = {
   };
   req: NextApiRequest;
   res: NextApiResponse;
-  action: (
-    params: ActionParams<T_Input>
-  ) => Promise<T_Output>;
+  action: (params: ActionParams<T_Input>) => Promise<T_Output>;
 };
 
 type ActionParams<T_Input> = {
-  req: NextApiRequest,
-  validatedInput: Awaited<T_Input>,
-  session?: Session,
-}
+  req: NextApiRequest;
+  validatedInput: Awaited<T_Input>;
+  session?: Session;
+};
 
 /**
  * This is a custom controller function that can be used to run API actions.
@@ -72,56 +70,64 @@ export const runController = async <
       ...validation.options,
     });
     // Run the action
-    const resBody = await action({req, validatedInput, session});
+    const resBody = await action({ req, validatedInput, session });
     res.status(200).json(resBody);
   } catch (e) {
-    // Return 400 when a validation error occurs
-    if (e instanceof ValidationError) {
-      res.status(400).json({
+    const error = e as Error;
+    const [statusCode, errorResponse] = getErrorResponse(req, error);
+    log(errorResponse.name, errorResponse.message, "error");
+    res.status(statusCode).json(errorResponse);
+  }
+};
+
+/**
+ * Analyzes the raw error and the request, and returns the appropriate response 
+ * status code and error response body.
+ * @param req Next.js HTTP Request
+ * @param e Raw error
+ * @returns [number, Error] The status code and error response body
+ */
+const getErrorResponse = (req: NextApiRequest, e: Error): [number, Error] => {
+  // Return 400 when a validation error occurs
+  if (e instanceof ValidationError) {
+    return [
+      400,
+      {
         name: "ValidationError",
         message: e.message,
-      });
-      return;
-    }
-    // Return 409 when a foreign key constraint violation occurs
-    if (
-      e instanceof Prisma.PrismaClientKnownRequestError &&
-      (isForeignKeyConstraintViolation(e) || isUniqueConstraintViolation(e))
-    ) {
-      log("ForeignKeyConstraintViolation", e.message);
-      res.status(409).json({
-        name: "ForeignKeyConstraintViolation",
-        message: "Violates a foreign key constraint, like uniqueness",
-      });
-      return;
-    }
-    // Return 400 when a missing related record error occurs
-    if (
-      e instanceof Prisma.PrismaClientKnownRequestError &&
-      isMissingRelatedRecord(e)
-    ) {
-      log("MissingRelatedRecord", e.message);
-      if (req.method === "DELETE") {
-        res.status(404).json({
-          name: "NotFound",
-          message: "The record does not exist",
-        });
-        return;
-      }
-      res.status(400).json({
-        name: "MissingRelatedRecord",
-        message: "One or more of the related records does not exist",
-      });
-      return;
-    }
-    // Catch ActionErrors and return them as JSON
-    if (e instanceof ActionError) {
-      res.status(e.getStatus()).json(e.toJSON());
-      return;
-    }
-    // Return 500 when an unexpected error occurs
-    const error = e as Error;
-    log("Unexpected", error.message);
-    res.status(500).json(error);
+      },
+    ];
   }
+  // Return 409 when a foreign key constraint violation occurs
+  if (
+    e instanceof Prisma.PrismaClientKnownRequestError &&
+    (isForeignKeyConstraintViolation(e) || isUniqueConstraintViolation(e))
+  ) {
+    return [409,{
+      name: "ForeignKeyConstraintViolation",
+      message: "Violates a foreign key constraint, like uniqueness",
+    }];
+  }
+  // Return 400 when a missing related record error occurs
+  if (
+    e instanceof Prisma.PrismaClientKnownRequestError &&
+    isMissingRelatedRecord(e)
+  ) {
+    if (req.method === "DELETE") {
+      return [404, {
+        name: "NotFound",
+        message: "The record does not exist",
+      }];
+    }
+    return [400, {
+      name: "MissingRelatedRecord",
+      message: "One or more of the related records does not exist",
+    }];
+  }
+  // Catch ActionErrors and return them as JSON
+  if (e instanceof ActionError) {
+    return [e.getStatus(), e.toJSON()];
+  }
+  // Return 500 when an unexpected error occurs
+  return [500, e];
 };
