fc5483b1c0af9ceb0fc5b364ce50622a5665c867
Daniel Beltran <danilidonbeltran@gmail.com>
2023-04-29 22:26:13 +0200

    fixed dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,38 @@
 import "@/styles/globals.css";
+import { useMemo } from "react";
 import type { AppProps } from "next/app";
 import { SessionProvider, SessionProviderProps } from "next-auth/react";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/system";
+import { createTheme, useMediaQuery } from "@mui/material";
+import { purple, green } from "@mui/material/colors";
 
 export default function App({
   Component,
   pageProps,
   session,
 }: AppProps & SessionProviderProps) {
+  // Enabling Dark Mode according to system-wide setting
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+          primary: purple,
+          secondary: green,
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <SessionProvider session={session}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline enableColorScheme />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline enableColorScheme />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
