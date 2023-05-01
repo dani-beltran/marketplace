63b484cdb75fc2e7280cce3270e964566b72a4d4
Daniel Beltran <danilidonbeltran@gmail.com>
2023-05-01 16:54:29 +0200

    updated seeding

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,4 +1,5 @@
 var Prisma = require("../src/lib/prisma-client");
+var Joda = require("@js-joda/core");
 
 console.info("Seeding DB...");
 console.info("URL: ", process.env.DATABASE_URL);
@@ -18,27 +19,87 @@ async function runSeeding() {
   try {
     // Reset DB
     await db.$executeRaw`TRUNCATE TABLE "User" CASCADE;`;
-    // Insert Data
-    const res = await db.$queryRaw`
-      INSERT INTO "User" ("name", "email", "image") 
-      VALUES('Pepe', 'pepe@dummy-mail.com', 'https://ih1.redbubble.net/image.3287754550.6742/st,small,507x507-pad,600x600,f8f8f8.u2.jpg')
-      RETURNING "id"
-    `;
-    const userId = res[0].id;
-    let promises = [];
-    for (let i = 0; i < 60; i++) {
-      const name = `Issue #${i}`;
-      const description = 'Lorem impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-      const query = db.$executeRaw`
-        INSERT INTO "Job" ("name", "description", "userId", "issueUrl") 
-        VALUES(${name}, ${description}, ${userId}, 'https://github.com/dani-beltran/marketplace/issues/1')
-      `;
-      promises.push(query);
-    }
-    await Promise.all(promises);
+    // Insert users
+    const clientWallet = await db.wallet.create({
+      data: {
+        balance: "1000.00",
+        user: {
+          create: {
+            name: "Pepe",
+            email: "pepe@dummy-mail.com",
+            image:
+              "https://ih1.redbubble.net/image.3287754550.6742/st,small,507x507-pad,600x600,f8f8f8.u2.jpg",
+          },
+        },
+      },
+    });
+    const contractorWallet = await db.wallet.create({
+      data: {
+        balance: "0.00",
+        user: {
+          create: {
+            name: "Juan",
+            email: "juan@dummy-mail.com",
+          },
+        },
+      },
+    });
+    const clientId = clientWallet.userId;
+    const contractorId = contractorWallet.userId;
+    // Insert jobs with no contracts
+    const nJobs = 60;
+    const inputJobs = new Array(nJobs).fill(0).map((_, i) => (
+      {
+        name: `Issue #${i}`,
+        description:
+          "Lorem impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        user: {connect: {id: clientId}},
+      }));
+    await createMany(db, db.job, inputJobs);
+    // Insert contracts
+    const contract = await db.contract.create({data: {
+      client: {connect: {id: clientId}},
+      contractor: {connect: {id: contractorId}},
+      name: `The Contract`,
+      terms: "none",
+      startDate: new Date(),
+      endDate: new Date(Joda.LocalDate.now().plusDays(30).toString()),
+      totalCost: Math.round(Math.random() * 1000).toString(),
+      job: {
+        create: {
+          name: `The Issue`,
+          description: 'none',
+          user: {connect: {id: clientId}},
+        }
+      }
+    }});
+    // Insert invoices
+    const nInvoices = 30;
+    const inputInvoices = new Array(nInvoices).fill(0).map((_, i) => (
+      {
+        number: `INV-${i}`,
+        date: new Date(),
+        dueDate: new Date(Joda.LocalDate.now().plusDays(30).toString()),
+        subtotal: Math.round(Math.random() * 1000).toString(),
+        contract: {connect: {id: contract.id}},
+      }
+    ));
+    await createMany(db, db.invoice, inputInvoices);
+
     db.$disconnect();
   } catch (e) {
     db.$disconnect();
     throw e;
   }
+}
+
+/**
+ * Creates many records in a single transaction.
+ * This is a workaround for the lack of batch operations in Prisma that returns 
+ * the created records.
+ */
+function createMany(client, model, records) {
+  return client.$transaction(
+    records.map((record) => model.create({ data: record })),
+  );
 }
